6d926e9c9b679d641f846d7e47fc7acd4a00c07a
Danilo França <danilo.franca01@gmail.com>
2022-04-23 04:07:46 +0000

    pequenas alterações

--- .config/rofi/bin/powermenu.sh
+++ .config/rofi/bin/powermenu.sh
@@ -30,7 +30,7 @@ case $chosen in
 		    if [[ -f /usr/bin/i3lock ]]; then
 			    i3lock
 		    elif [[ -f /usr/bin/betterlockscreen ]]; then
-		  	  betterlockscreen -l
+		  	  betterlockscreen --lock
 				fi
     ;;
     $suspend)
